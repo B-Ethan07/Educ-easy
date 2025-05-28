c2ca241ad4d555adbe6d652ea5373d2561ebc6df
Ethan B <e.boulard07@gmail.com>
2025-05-28 11:10:59 +0200

    Start JS + 3rd page

--- popup_news.js
+++ popup_news.js
@@ -1,1 +1,27 @@
 
+function afficherPopup() {
+    let popupBackground = document.querySelector(".popupBackground")
+    popupBackground.classList.add("active")
+}
+
+function cacherPopup() {
+    let popupBackground = document.querySelector(".popupBackground")
+    popupBackground.classList.remove("active")
+}
+
+
+function initAddEventListenerPopup() {
+    let btnPartage = document.querySelector("#btnPartage")
+    let popupBackground = document.querySelector(".popupBackground")
+    btnPartage.addEventListener("click", () => {
+        console.log('bouton cliquer')
+        afficherPopup()
+    })
+
+    popupBackground.addEventListener("click", (event) => {
+        if (event.target === popupBackground) {
+            cacherPopup()
+        }
+    })
+}
+
